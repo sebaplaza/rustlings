33d045766d33a71832df9739535c5a16cccba1b2
plaza-s <plaza-s@mgdis.fr>
2021-12-05 21:45:38 +0100

    feat: until modules1

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -4,14 +4,13 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute the command `rustlings hint if2` if you want a hint :)
 
-// I AM NOT DONE
-
 pub fn fizz_if_foo(fizzish: &str) -> &str {
-    if fizzish == "fizz" {
-        "foo"
-    } else {
-        1
-    }
+    let result = match fizzish {
+        "fizz" => "foo",
+        "fuzz" => "bar",
+        _ => "baz",
+    };
+    result
 }
 
 // No test changes needed!
