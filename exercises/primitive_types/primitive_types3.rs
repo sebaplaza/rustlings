9b5dabe62833eb9d5e46ace56765099f2af3a5bd
Sebastian Plaza <sebastianplaza@gmail.com>
2023-02-12 21:42:11 +0100

    feat: move_semantics4

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,10 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // both are valid
+    let a = ["one hundred"; 100];
+    let a = 0..60;
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
